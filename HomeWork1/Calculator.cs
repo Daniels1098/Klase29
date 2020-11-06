23a4b72d6dafcd9eefb720de5be5087802648fea
daniels1098 <daniels1098@gmail.com>
2020-11-06 15:40:18 +0200

    Šķiet ka līdz galam nepareizi izpildīti uzd norādijumi.

---
+++ HomeWork1/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HomeWork1
+{
+    class Calculator
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ievadi skaitli x: ");
+            Utilis.getValue();    
+        }
+    }
+}
---
+++ HomeWork1/Utilis.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+
+namespace HomeWork1
+{
+    class Utilis
+    {
+        public static void getValue()
+        {
+            float x = float.Parse(Console.ReadLine());
+            Console.Write("Ievadi skaitli y: ");
+            float y = float.Parse(Console.ReadLine());
+            Console.Write("Ievadi aritmētisko operāciju (+, -, / vai *): ");
+            string u = Console.ReadLine();
+            if (u.Equals("+"))
+            {
+                 Console.WriteLine($"{x} + {y} = {x + y}");
+            }
+            if (u.Equals("-"))
+            {
+                Console.WriteLine($"{x} - {y} = {x - y}");
+            }
+            if (u.Equals("/"))
+            {
+                Console.WriteLine($"{x} / {y} = {x / y}");
+            }
+            if (u.Equals("*"))
+            {
+                Console.WriteLine($"{x} * {y} = {x * y}");
+            } 
+            else
+            {
+                Console.WriteLine("Sistēma neatpazīst simbolu");
+            }
+        }
+    }
+}
+      
+            
+
+
+
+
+    
